49b3552215f8d0c47ed47bb38d3011e203c8c088
MuhammadBilalEllahi <bilalillahi25@gmail.com>
2025-10-19 14:10:44 +0500

    Refactor video path handling in example.py and update requirements to include streamlit

--- example.py
+++ example.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from sorawm.core import SoraWM
 
 if __name__ == "__main__":
-    input_video_path = Path("resources/dog_vs_sam.mp4")
-    output_video_path = Path("outputs/sora_watermark_removed.mp4")
+    fileName = "finding_milk"
+    input_video_path = Path(f"outputs/{fileName}_sora_watermark_removed.mp4")
+    output_video_path = Path(f"outputs/{fileName}_sora_watermark_removed_refine.mp4")
     sora_wm = SoraWM()
     sora_wm.run(input_video_path, output_video_path)
